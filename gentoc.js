de172aad70e21cb28619eda05b3e09d809308b6f
yapcheahshen <yapcheahshen@gmail.com>
2016-06-23 00:33:47 +0800

    gentoc

--- gentoc.js
+++ gentoc.js
@@ -1,2 +1,14 @@
 /*generate toc */
-f
+var fs=require("fs");
+var files=fs.readFileSync("./xml/files.lst","utf8").split(/\r?\n/);
+var sourcepath="./genxml/";
+files.shift();//ignore 000
+var processfile=function(fn){
+	var content=fs.readFileSync(sourcepath+fn,"utf8");
+
+	content.replace(/<h(\d+) t="(.+?)">(.+?)<\/h/g,function(m,d,t,text){
+		console.log(fn.substr(0,3)+"\t"+d+"\t"+t+text);
+	});
+
+}
+files.map(processfile)
