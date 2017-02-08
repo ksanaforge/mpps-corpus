f52e0e39ac8b7903a1ca9f2213399eb7741a901d
yapcheahshen <yapcheahshen@gmail.com>
2017-02-08 22:33:17 +0800

    generate kepan fields for ksana-corpus-view

--- comparetaisho.js
+++ comparetaisho.js
@@ -38,9 +38,9 @@ const compressdiff=function(D,pb){
 	for (var i=0;i<D.length;i++) {
 		const d=D[i];
 		if (d.added) {
-			p1+=d.count;
-		} else if (d.removed) {
 			p2+=d.count;
+		} else if (d.removed) {
+			p1+=d.count;
 		} else {
 			out1.push(p1);
 			out2.push(p2);
@@ -69,25 +69,26 @@ const compare=function(rawpb,text1,text2){
 	
 }
 
-const translate=function(t1pos,t2pos,pos){
+const translate=function(t1pos,t2pos,pos,text1,text2){
 	const at=bsearch(t1pos,pos,true)-1;
+	
 	const dist=pos-t1pos[at];
 	const t2dist=t2pos[at+1]-t2pos[at];
-	if (t2dist>dist) {
+	if (t2dist>=dist) {
 		return t2pos[at]+dist;
 	} else {
-		return -(t2pos[at]+dist);
+		return (t2pos[at]+t2dist) ;
 	}
 }
 
-const convert=function(pbdiffs,standoffs){
+const convert=function(pbdiffs,standoffs,text1,text2){
 	
 	for (var i=0;i<standoffs.length;i++) {
 		const pb=standoffs[i][0];
 		const pos=standoffs[i][1];
 		const diffs=pbdiffs[pb];
-
-		standoffs[i].push(translate(diffs[0],diffs[1],pos));
+		if (!diffs) break;
+		standoffs[i].push(translate(diffs[0],diffs[1],pos,text1,text2));
 	}
 	return standoffs;
 }
@@ -96,17 +97,20 @@ const check=function(rawpb,text1,text2,out){
 		const pb=out[i][0];
 		const p1=out[i][1];
 		const p2=out[i][2];
-		const at=bsearch(rawpb,pb);
+		const at=rawpb.indexOf(pb);
 		const t1=text1[at],t2=text2[at];
 		console.log(pb);
 		console.log(p1,t1.substr(p1,10))
-		console.log(p2,t2.substr(p2,10))
+		console.log(p2,t2.substr(Math.abs(p2),10))
 	}
 	
 }
+
 const fetch=function(cor) {
-	const allpb=[],text1=[],rawpb=[];
-	const count= mpps_taisho.length;
+	var text1=[],text2=[];
+
+	const allpb=[],rawpb=[];
+	const count=mpps_taisho.length;
 	console.log("fetch count",mpps_taisho.length)
 	for (var i=0;i<count;i++) {
 		rawpb.push(mpps_taisho[i][0]);
@@ -117,23 +121,27 @@ const fetch=function(cor) {
 	cor.getText(allpb,function(texts){
 
 		for (var i=0;i<texts.length;i++){
-			texts[i]=texts[i].join("\n");
+			text2[i]=texts[i].join("\n");
 		}
 		console.timeEnd("fetch")
-		const diffs=compare(rawpb,text1,texts);
-
+		const diffs=compare(rawpb,text1,text2);
 		console.log("start convert");
-		const new_standoffs=convert(diffs,mpps_standoffs);
+		
+		const new_standoffs=convert(diffs,mpps_standoffs,text1,text2);
 
 		console.log("write to mpps_standoffs_converted")
-		fs.writeFileSync("mpps_standoffs_converted.js",JSON.stringify(new_standoffs,""," "),"utf8");
-		/*
+		fs.writeFileSync("mpps_standoffs_converted.js","module.exports="+JSON.stringify(new_standoffs,""," "),"utf8");
+		
+		//return 
+
 		output=convert(diffs,[
-			//["25.58a" ,43],
-			["25.58a" ,202]
+			["25.58a" ,43],
+			["25.58a" ,202],
+			["25.346a" ,103]
 			]);
-		check(rawpb,texts,text1,output);
-		*/
+		
+		check(rawpb,text1,text2,output);
+		
 	});
 
 }
---
+++ genfields.js
@@ -0,0 +1,63 @@
+/*	generate external kdb fields from mpps_standoffs_converted.js */
+const standoffs=require("./mpps_standoffs_converted");
+const {openCorpus,bsearch}=require("ksana-corpus");
+var cor;
+var output={},prevpage=0,pagerange,pagetext=null,linebreaks;
+const fs=require("fs");
+/*group by article */
+const calKPos=function(realpos){
+	const line=bsearch(linebreaks,realpos,true)-1;
+	const ch=realpos-linebreaks[line];
+	if (!pagetext[line]) {
+		console.log("error realpos",realpos);
+		return 0;
+	}
+	const kch=cor.kcount(  pagetext[line].substr(0,ch));
+	return cor.makeKPos([pagerange.startarr[0],pagerange.startarr[1],line,kch]);
+}
+const processtag=function(standoff){
+	const k=cor.parseRange(standoff[0].replace(".","p")+"0100");
+	const article=cor.articleOf(k.start);
+	if (!output[article.at]) output[article.at]={pos:[],value:[]};
+
+	pagerange=cor.parseRange(standoff[0].replace(".","p")+"0100-2931");
+	
+	if (prevpage!==standoff[0]) {
+
+		pagetext=cor.getText(pagerange.kRange);
+		var len=0;
+		linebreaks=pagetext.map(function(l){
+			len+=l.length+1;
+			return len;
+		});
+		linebreaks.unshift(0);
+		prevpage=standoff[0];
+	}
+	if (!standoff[3]) return;
+	realpos=standoff[3];
+	if (realpos<0) realpos=-realpos;//closeest guest
+
+
+	standoff[2].replace(/<H(\d+).*?>(.*?)<\/H\d>/,function(m,depth,head){
+		const kpos=calKPos(realpos);
+		if (kpos) {
+			output[article.at].pos.push(kpos);
+			output[article.at].value.push(depth+"\t"+head);			
+		} else {
+			console.log("wrong pos",head,realpos,prevpage);
+		}
+	})
+
+}
+
+openCorpus("taisho",function(err,_cor){
+	cor=_cor;
+	const alltext=cor.parseRange('25p57a0100-756c2919'); //range of MPPS
+	cor.getText(alltext.kRange,function(text){
+
+		standoffs.forEach(processtag);	
+		fs.writeFileSync("mpps_fields.js",JSON.stringify(output),"utf8");
+		
+	});
+});
+
